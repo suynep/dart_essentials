347b3773d61b96963584b63cfccd904ebb58e230
suywho <suy.nepal@gmail.com>
2026-02-25 10:01:51 +0545

    feat: add problem chooser logic
    Allows for choosing problems using the available choices list

--- bin/week_2_problems.dart
+++ bin/week_2_problems.dart
@@ -1,11 +1,44 @@
-import 'package:week_2_problems/week_2_problems.dart' as week_2_problems;
+import 'dart:io';
+
 import '../problems/one.dart';
 import '../problems/two.dart';
 import '../problems/three.dart';
 import '../problems/chooseable_problem.dart';
 
 void main(List<String> arguments) {
-  solveProblem(ProblemThree());
+  chooseProblem();
+}
+
+void chooseProblem() {
+  stdout.write("""
+1: Solve Problem 1,
+2: Solve Problem 2,
+3: Solve Problem 3,
+
+""");
+  bool _continue = true;
+
+  while (_continue) {
+    stdout.write("Enter a choice: ");
+    String _input = stdin.readLineSync() ?? "";
+
+    switch (_input.trim()) {
+      case "1":
+        solveProblem(ProblemOne());
+        _continue = false;
+        break;
+      case "2":
+        solveProblem(ProblemTwo());
+        _continue = false;
+        break;
+      case "3":
+        solveProblem(ProblemThree());
+        _continue = false;
+        break;
+      default:
+        stdout.write("Invalid Option! Try again\n");
+    }
+  }
 }
 
 void solveProblem(IChooseableProblem problem) {
