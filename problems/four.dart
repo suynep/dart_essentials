f108169e333860608cf239537e9c6c83c66367dd
suywho <suy.nepal@gmail.com>
2026-02-25 10:05:34 +0545

    feat: initiate problem no. 4 implementation

---
+++ problems/four.dart
@@ -0,0 +1,15 @@
+// ### Project 4: Shopping Cart
+// Create a program that:
+// - Manages items with name, price, quantity
+// - Uses Lists and Maps
+// - Calculates total price
+// - Applies discounts
+// - Generates receipt
+
+import 'chooseable_problem.dart';
+
+class ProblemFour implements IChooseableProblem {
+  void getData() {}
+  void wrangleAndCompute() {}
+  void display() {}
+}
