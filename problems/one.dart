92e19897e1eb9cd7cdd9a5dcc24b076764c455df
suywho <suy.nepal@gmail.com>
2026-02-26 11:14:49 +0545

    feat: add input parse guards

--- problems/one.dart
+++ problems/one.dart
@@ -11,22 +11,141 @@ import "dart:io";
 import "dart:math";
 
 class ProblemOne implements IChooseableProblem {
-  late final List<int> input;
+  List<int> input = [];
   late final Map<String, dynamic> outputs = {};
 
   @override
   void getData() {
-    stdout.write("Enter comma-separated list of nums: ");
-    String? userInput = stdin.readLineSync();
-    userInput = userInput ?? ""; // handle potential nullability
+    bool valid = true;
 
-    input = userInput.split(",").toList().map((ele) {
-      var numericEle = int.tryParse(ele.trim()) ?? 0;
-      return numericEle;
-    }).toList();
+    do {
+      input = [];
+      valid = true;
+      stdout.write("Enter comma-separated list of nums: ");
+      String? userInput = stdin.readLineSync();
+      userInput = userInput ?? ""; // handle potential nullability
+
+      if (userInput.isEmpty) {
+        print("Empty Input Detected!");
+        valid = false;
+      }
+
+      if (userInput.contains(RegExp(r", *$"))) {
+        print("Please remove any trailing commas");
+        valid = false;
+      }
+
+      if (userInput.contains(RegExp(r"[0-9] +[0-9]"))) {
+        print("Please enter in CSV format!");
+        valid = false;
+      }
+
+      if (valid) {
+        for (var ele in userInput.split(",").toList()) {
+          int? numericEle = int.tryParse(ele.trim());
+          if (numericEle == null) {
+            valid = false;
+            print("Non-numeric data detected!");
+            break;
+          }
+          input.add(numericEle);
+        }
+      }
+    } while (!valid);
   }
 
-  /// call this AFTER calling getData, as that initializes out late input instance var
+  /*
+  1: Sum
+  2: Average
+  3: Minimum
+  4: Maximum
+  5: Odds
+  6: Evens
+  */
+
+  void getComputationChoice() {
+    print("""
+
+1: Sum
+2: Average
+3: Minimum
+4: Maximum
+5: Odds
+6: Evens
+
+q: exit
+""");
+
+    bool valid = true;
+
+    do {
+      valid = true;
+      stdout.write("Enter a choice: ");
+      String? choiceInput = stdin.readLineSync();
+      int integralInput = 0;
+
+      if (choiceInput == null) {
+        print("Invalid Choice Detected!");
+        valid = false;
+      } else {
+        try {
+          integralInput = int.parse(choiceInput.trim());
+        } on FormatException catch (e) {
+          if (choiceInput.trim().toLowerCase() != "q") {
+            print("Cannot Parse Choice");
+            valid = false;
+          } else {
+            break;
+          }
+        }
+      }
+
+      if (valid) {
+        switch (integralInput) {
+          case 1:
+            display("Sum");
+            valid = false;
+            break;
+          case 2:
+            display("Average");
+            valid = false;
+            break;
+          case 3:
+            display("Minimum");
+            valid = false;
+            break;
+          case 4:
+            display("Maximum");
+            valid = false;
+            break;
+          case 5:
+            display("Odds");
+            valid = false;
+            break;
+          case 6:
+            display("Evens");
+            valid = false;
+            break;
+          default:
+            print("Invalid Option");
+            valid = false;
+            break;
+        }
+      } else {
+        switch ((choiceInput ?? "").trim().toLowerCase()) {
+          case "q":
+            valid = true;
+            break;
+          default:
+            print("Press `q` to exit");
+            valid = false;
+            break;
+        }
+      }
+    } while (!valid);
+  }
+
+  /// call this AFTER calling getData, as that initializes our late input instance var
   @override
   void wrangleAndCompute() {
     int sum = input.reduce((v, e) => v + e);
@@ -68,13 +187,25 @@ class ProblemOne implements IChooseableProblem {
 
     outputs["Ascending"] = ascSortedInput;
     outputs["Descending"] = descSortedInput;
+
+    getComputationChoice();
   }
 
-  /// Displays output; call after wrangleAndCompute
+  /// Displays output; call after `ProblemOne.wrangleAndCompute()`.
+  ///
+  /// The optional parameter `key` can be provided to display specific output data.
   @override
-  void display() {
-    for (var k in outputs.keys) {
-      print("$k: ${outputs[k]}");
+  void display([String? key]) {
+    if (key != null) {
+      if (outputs.keys.contains(key)) {
+        print("$key: ${outputs[key]}");
+      } else {
+        print("The key doesn't exist!");
+      }
+    } else {
+      for (var k in outputs.keys) {
+        print("$k: ${outputs[k]}");
+      }
     }
   }
 }
